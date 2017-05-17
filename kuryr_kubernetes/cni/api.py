5fdcefba70e767a86164928ea17f5e79e5c43dda
Antoni Segura Puimedon <antonisp@celebdor.com>
2017-05-17 11:55:52 +0200

    logging: Improve CNI debug logging:
    Without this patch, when successfully exiting the CNI runner, we'd log
    something like:
    
        2017-05-16 16:45:11.763 32397 DEBUG kuryr_kubernetes.cni.main [-] Exiting with status None run /usr/lib/python2.7/site-packages/kuryr_kubernetes/cni/main.py:96
    
    After this change, we report the real exit status we'll give to the
    kubelet:
    
        2017-05-16 16:56:14.750 33914 DEBUG kuryr_kubernetes.cni.main [-] Exiting with status 0 run /usr/lib/python2.7/site-packages/kuryr_kubernetes/cni/main.py:96
    
    Change-Id: I10045ffa55128fe70151a5427d82535425a90ed4
    Signed-off-by: Antoni Segura Puimedon <antonisp@celebdor.com>

--- kuryr_kubernetes/cni/api.py
+++ kuryr_kubernetes/cni/api.py
@@ -90,6 +90,7 @@ class CNIRunner(object):
             else:
                 raise k_exc.CNIError(_("unknown CNI_COMMAND: %s")
                                      % params.CNI_COMMAND)
+            return 0
         except Exception as ex:
             # LOG.exception
             self._write_exception(fout, str(ex))
