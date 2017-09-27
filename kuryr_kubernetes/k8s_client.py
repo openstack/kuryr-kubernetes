673b3465b97d1a8cb679df5d7958c917592c0245
Jiyuan Tang <tangjiyuan@caicloud.io>
2017-09-27 15:41:39 +0800

    Fixed error: metadata.resourceVersion: Invalid value: \"\": must be specified for an update when annotating

--- kuryr_kubernetes/k8s_client.py
+++ kuryr_kubernetes/k8s_client.py
@@ -94,12 +94,20 @@ class K8sClient(object):
         if self.token:
             header.update({'Authorization': 'Bearer %s' % self.token})
         while itertools.count(1):
-            data = jsonutils.dumps({
-                "metadata": {
-                    "annotations": annotations,
-                    "resourceVersion": resource_version,
-                }
-            }, sort_keys=True)
+            if resource_version:
+                data = jsonutils.dumps({
+                    "metadata": {
+                        "annotations": annotations,
+                        "resourceVersion": resource_version,
+                    }
+                }, sort_keys=True)
+            else:
+                data = jsonutils.dumps({
+                    "metadata": {
+                        "annotations": annotations
+                    }
+                }, sort_keys=True)
+
             response = requests.patch(url, data=data,
                                       headers=header, cert=self.cert,
                                       verify=self.verify_server)
@@ -121,6 +129,11 @@ class K8sClient(object):
                 LOG.debug("Annotations for %(path)s already present: "
                           "%(names)s", {'path': path,
                                         'names': retrieved_annotations})
+
+            LOG.debug("Exception response, headers: %(headers)s, "
+                      "content: %(content)s, text: %(text)s"
+                      % {'headers': response.headers,
+                         'content': response.content, 'text': response.text})
             raise exc.K8sClientException(response.text)
 
     def watch(self, path):
