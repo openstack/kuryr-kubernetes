e83627244fea9186d0cb8770bdf9a283a5deb07b
Michał Dulko <mdulko@redhat.com>
2017-10-09 15:32:20 +0200

    Remove 99-loopback.conf
    Turns out 99-loopback.conf file isn't really required when running
    kuryr-kubernetes. This commit removes its installation from DevStack
    plugin and CNI Docker image.
    
    Change-Id: I8f2097287df907675c4113cd225a7ee9f6cd7ef1

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -437,9 +437,6 @@ function prepare_kubelet {
     sudo install -o "$STACK_USER" -m 0664 -D \
         "${KURYR_HOME}${kubelet_plugin_dir}/10-kuryr.conf" \
         "${CNI_CONF_DIR}/10-kuryr.conf"
-    sudo install -o "$STACK_USER" -m 0664 -D \
-        "${KURYR_HOME}${kubelet_plugin_dir}/99-loopback.conf" \
-        "${CNI_CONF_DIR}/99-loopback.conf"
 }
 
 function run_k8s_kubelet {
