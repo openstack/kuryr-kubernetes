6dfd4067f553f3a2f39b7c95bb0812dc8f8be7c9
Daniel Mellado <dmellado@redhat.com>
2018-10-08 05:16:17 -0400

    Add security groups driver for NP
    This commit creates a new security group driver for handling network
    policies sg and sg rules.
    
    Partially Implements: blueprint k8s-network-policies
    Change-Id: Ie4dfac39704f4bbfb31eb329cd43ab8a06addf0d

---
+++ kuryr_kubernetes/controller/drivers/network_policy_security_groups.py
@@ -0,0 +1,125 @@
+# Copyright 2018 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from six.moves.urllib.parse import urlencode
+
+from kuryr_kubernetes import clients
+from kuryr_kubernetes import config
+from kuryr_kubernetes import constants
+from kuryr_kubernetes.controller.drivers import base
+from kuryr_kubernetes import exceptions
+
+from oslo_config import cfg
+from oslo_log import log as logging
+
+LOG = logging.getLogger(__name__)
+
+
+def _get_kuryrnetpolicy_crds(labels=None, namespace='default'):
+    kubernetes = clients.get_kubernetes_client()
+    try:
+        if labels:
+            LOG.debug("Using labels %s", labels)
+            labels.pop('pod-template-hash', None)
+            # removing pod-template-hash is necessary to fetch the proper list
+            labels = urlencode(labels)
+            knp_path = '{}/{}/kuryrnetpolicies?labelSelector={}'.format(
+                constants.K8S_API_CRD_NAMESPACES, namespace, labels)
+            LOG.debug("K8s API Query %s", knp_path)
+            knps = kubernetes.get(knp_path)
+            LOG.debug("Return Kuryr Network Policies with label %s", knps)
+        else:
+            knps = kubernetes.get('{}/{}/kuryrnetpolicies'.format(
+                constants.K8S_API_CRD_NAMESPACES, namespace))
+    except exceptions.K8sResourceNotFound:
+        LOG.exception("KuryrNetPolicy CRD not found")
+        raise
+    except exceptions.K8sClientException:
+        LOG.exception("Kubernetes Client Exception")
+        raise
+    return knps
+
+
+class NetworkPolicySecurityGroupsDriver(base.PodSecurityGroupsDriver):
+    """Provides security groups for pods based on network policies"""
+
+    def get_security_groups(self, pod, project_id):
+        sg_list = []
+        pod_namespace = pod['metadata']['namespace']
+        pod_labels = pod['metadata'].get('labels')
+        LOG.debug("Using labels %s", pod_labels)
+
+        knp_crds = _get_kuryrnetpolicy_crds(pod_labels,
+                                            namespace=pod_namespace)
+
+        for crd in knp_crds.get('items'):
+            LOG.debug("Appending %s", str(crd['spec']['securityGroupId']))
+            sg_list.append(str(crd['spec']['securityGroupId']))
+
+        knp_namespace_crds = _get_kuryrnetpolicy_crds(namespace=pod_namespace)
+
+        for crd in knp_namespace_crds.get('items'):
+            if not crd['metadata'].get('labels'):
+                LOG.debug("Appending %s", str(crd['spec']['securityGroupId']))
+                sg_list.append(str(crd['spec']['securityGroupId']))
+
+        if not sg_list:
+            sg_list = config.CONF.neutron_defaults.pod_security_groups
+            if not sg_list:
+                raise cfg.RequiredOptError('pod_security_groups',
+                                           cfg.OptGroup('neutron_defaults'))
+
+        return sg_list[:]
+
+    def create_namespace_sg(self, namespace, project_id, crd_spec):
+        LOG.debug("Security group driver does not create SGs for the "
+                  "namespaces.")
+        return {}
+
+    def delete_sg(self, sg_id):
+        LOG.debug("Security group driver does not implement deleting "
+                  "SGs.")
+
+
+class NetworkPolicyServiceSecurityGroupsDriver(
+        base.ServiceSecurityGroupsDriver):
+    """Provides security groups for services based on network policies"""
+
+    def get_security_groups(self, service, project_id):
+        sg_list = []
+        svc_namespace = service['metadata']['namespace']
+        svc_labels = service['metadata'].get('labels')
+        LOG.debug("Using labels %s", svc_labels)
+
+        knp_crds = _get_kuryrnetpolicy_crds(svc_labels,
+                                            namespace=svc_namespace)
+
+        for crd in knp_crds.get('items'):
+            LOG.debug("Appending %s" % str(crd['spec']['securityGroupId']))
+            sg_list.append(str(crd['spec']['securityGroupId']))
+
+        knp_namespace_crds = _get_kuryrnetpolicy_crds(namespace=svc_namespace)
+
+        for crd in knp_namespace_crds.get('items'):
+            if not crd['metadata'].get('labels'):
+                LOG.debug("Appending %s", str(crd['spec']['securityGroupId']))
+                sg_list.append(str(crd['spec']['securityGroupId']))
+
+        if not sg_list:
+            sg_list = config.CONF.neutron_defaults.pod_security_groups
+            if not sg_list:
+                raise cfg.RequiredOptError('pod_security_groups',
+                                           cfg.OptGroup('neutron_defaults'))
+
+        return sg_list[:]
---
+++ kuryr_kubernetes/tests/unit/controller/drivers/test_network_policy_security_groups.py
@@ -0,0 +1,119 @@
+# Copyright 2018 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import mock
+
+from kuryr_kubernetes.controller.drivers import network_policy_security_groups
+from kuryr_kubernetes.tests import base as test_base
+from kuryr_kubernetes.tests.unit import kuryr_fixtures as k_fix
+
+from oslo_config import cfg
+
+
+class TestNetworkPolicySecurityGroupsDriver(test_base.TestCase):
+
+    def setUp(self):
+        super(TestNetworkPolicySecurityGroupsDriver, self).setUp()
+        self._labels = mock.sentinel.labels
+        self._project_id = mock.sentinel.project_id
+        self._sg_id = mock.sentinel.sg_id
+        self._namespace = 'default'
+        self._crd = {
+            'metadata': {'name': mock.sentinel.name,
+                         'selfLink': mock.sentinel.selfLink},
+            'spec': {
+                'egressSgRules': [
+                    {'security_group_rule':
+                     {'description': 'Kuryr-Kubernetes NetPolicy SG rule',
+                      'direction': 'egress',
+                      'ethertype': 'IPv4',
+                      'port_range_max': 5978,
+                      'port_range_min': 5978,
+                      'protocol': 'tcp',
+                      'security_group_id': self._sg_id,
+                      'id': mock.sentinel.id
+                      }}],
+                'ingressSgRules': [
+                    {'security_group_rule':
+                     {'description': 'Kuryr-Kubernetes NetPolicy SG rule',
+                      'direction': 'ingress',
+                      'ethertype': 'IPv4',
+                      'port_range_max': 6379,
+                      'port_range_min': 6379,
+                      'protocol': 'tcp',
+                      'security_group_id': self._sg_id,
+                      'id': mock.sentinel.id
+                      }}],
+                'securityGroupId': self._sg_id,
+                'securityGroupName': mock.sentinel.sg_name}}
+
+        self._crds = {
+            "apiVersion": "v1",
+            "items": [self._crd],
+            "kind": "List",
+            "metadata": {
+                "resourceVersion": "",
+                "selfLink": mock.sentinel.selfLink}}
+
+        self._empty_crds = {
+            "apiVersion": "v1",
+            "items": [],
+            "kind": "List",
+            "metadata": {
+                "resourceVersion": "",
+                "selfLink": mock.sentinel.selfLink}}
+
+        self._pod = {
+            'apiVersion': 'v1',
+            'kind': 'Pod',
+            'metadata': {
+                'name': mock.sentinel.pod_name,
+                'namespace': self._namespace,
+                'labels': {
+                    'run': 'demo'}},
+            'spec': {
+                'containers': [{
+                    'image': 'kuryr/demo',
+                    'imagePullPolicy': 'Always',
+                    'name': mock.sentinel.pod_name
+                    }]
+                }}
+
+        self.kubernetes = self.useFixture(k_fix.MockK8sClient()).client
+        self._driver = (
+            network_policy_security_groups.NetworkPolicySecurityGroupsDriver())
+
+    @mock.patch.object(network_policy_security_groups,
+                       '_get_kuryrnetpolicy_crds')
+    def test_get_security_groups(self, m_get_crds):
+        m_get_crds.return_value = self._crds
+        self._driver.get_security_groups(self._pod, self._project_id)
+        m_get_crds.assert_called_with(namespace=self._namespace)
+
+    @mock.patch.object(network_policy_security_groups,
+                       '_get_kuryrnetpolicy_crds')
+    def test_get_security_groups_with_label(self, m_get_crds):
+        labels = {'run': 'demo'}
+        self._crds['metadata']['labels'] = labels
+        m_get_crds.return_value = self._crds
+        self._driver.get_security_groups(self._pod, self._project_id)
+        m_get_crds.assert_called()
+
+    @mock.patch.object(network_policy_security_groups,
+                       '_get_kuryrnetpolicy_crds')
+    def test_get_security_groups_no_crds(self, m_get_crds):
+        m_get_crds.return_value = self._empty_crds
+        self.assertRaises(cfg.RequiredOptError,
+                          self._driver.get_security_groups, self._pod,
+                          self._project_id)
